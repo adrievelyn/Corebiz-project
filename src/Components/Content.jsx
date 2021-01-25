456400ec01109cb874e84b57f8c239250ee638c1
adrievelyn <adrievelyn@gmail.com>
2021-01-25 17:09:54 -0300

    agregando carousel de imágenes a content

---
+++ src/Components/Content.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import ContainerCss from '../styles/Container.css'
+
+
+class Content extends React.Component{
+	render(){
+		return(
+			<section class='carousel'>
+		<div class='carousel__container'>
+			<div class='carousel-item'>
+				<img class='carousel-item__img' src="" alt=""></img>
+				<div class='carousel-item__details'>
+					<div>
+					<img src=""/>
+					<img src=""/>
+					</div>
+				</div>
+				<p class='carousel-item__details--title'></p>
+				<p class='carousel-item__details--subtitle'></p>
+				<img class='carousel-item__img' src="" alt=""></img>
+				<div class='carousel-item__details'>
+					<div>
+					<img src=""/>
+					<img src=""/>
+					</div>
+				</div>
+				<p class='carousel-item__details--title'></p>
+				<p class='carousel-item__details--subtitle'></p>
+				<img class='carousel-item__img' src="" alt=""></img>
+				<div class='carousel-item__details'>
+					<div>
+					<img src=""/>
+					<img src=""/>
+					</div>
+				</div>
+				<p class='carousel-item__details--title'></p>
+				<p class='carousel-item__details--subtitle'></p>
+				<img class='carousel-item__img' src="" alt=""></img>
+				<div class='carousel-item__details'>
+					<div>
+					<img src=""/>
+					<img src=""/>
+					</div>
+				</div>
+				<p class='carousel-item__details--title'></p>
+				<p class='carousel-item__details--subtitle'></p>
+				<img class='carousel-item__img' src="" alt=""></img>
+				<div class='carousel-item__details'>
+					<div>
+					<img src=""/>
+					<img src=""/>
+					</div>
+				</div>
+				<p class='carousel-item__details--title'></p>
+				<p class='carousel-item__details--subtitle'></p>
+				<img class='carousel-item__img' src="" alt=""></img>
+				<div class='carousel-item__details'>
+					<div>
+					<img src=""/>
+					<img src=""/>
+					</div>
+				</div>
+				<p class='carousel-item__details--title'></p>
+				<p class='carousel-item__details--subtitle'></p>
+				
+			</div>
+		</div>
+	</section>
+		)
+	}
+}
+
+
+export default Content;
